f2f87c693bc2cbe7e1618e4d5adac632e846c474
Mary Dang <dangmaryk@gmail.com>
2019-04-25 19:58:48 -0500

    added all logic for database & updated README

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 # Scrapey
 
-[Watch this demo of a possible submission](https://youtu.be/4ltZr3VPmno). See the deployed demo application [here](http://nyt-mongo-scraper.herokuapp.com/).
+An app for Kpop lovers that lets users view and comment on news that was scraped from AllKpop.
 
-## Instructions
-
-  2. Users should also be able to leave comments on the articles displayed and revisit them later. The comments should be saved to the database as well and associated with their articles. Users should also be able to delete comments left on articles. All stored comments should be visible to every user.
+[Deploy Link](https://md-scrappy.herokuapp.com/)
 
 ---
--- controller/mongoHeadlines.js
+++ controller/mongoHeadlines.js
@@ -49,7 +49,46 @@ router.get("/scrape", function(req,res) {
   });
 });
 
+// Route for getting all Articles from the db
+router.get("/articles", function(req, res) {
+  db.Article.find({})
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
 
+// Route for grabbing a specific Article by id, populate it with it's note
+router.get("/articles/:id", function(req, res) {
+  db.Article.findOne({ _id: req.params.id })
+    .populate("Note")
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
+// Route for saving/updating an Article's associated Note
+router.post("/articles/:id", function(req, res) {
+  db.Note.create(req.body)
+    .then(function(dbNote) {
+      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    })
+    .then(function(dbArticle) {
+      // If we were able to successfully update an Article, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
 
 module.exports = router;
 
